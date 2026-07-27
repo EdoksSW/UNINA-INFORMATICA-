5b4727dfed9fb6c30a4c313eb5ffa6941857156d
EdoksSW <e.peiko@studenti.unina.it>
2026-07-27 11:59:55 +0200

    ProgrammazioneInJava

--- UNINA(TerzoCanale)/Breve(Prog.Java)/java/CoseNuove/Polimorfismo/src/Boat.java
+++
@@ -1,8 +0,0 @@
-public class Boat extends Vehicle
-{
-    @Override
-    void go()
-    {
-        System.out.println("You seal the boat");
-    }
-}
--- UNINA(TerzoCanale)/Breve(Prog.Java)/java/CoseNuove/runtime_Polymorphism/src/Cat.java
+++
@@ -1,8 +0,0 @@
-public class Cat extends Animal
-{
-    @Override
-    void speak()
-    {
-        System.out.println("The dog goeas *miao*");
-    }
-}
